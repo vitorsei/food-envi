9224604b8ce0daa7193507d7b7ecbc5e1372a49e
vitor <seiji.vitor@gmail.com>
2017-12-14 22:56:55 +1100

    Bookmarks

---
+++ js/data.js
@@ -0,0 +1,34 @@
+var dishesMap = [
+  {
+    id: '0',
+    img: 'fried-rice.PNG',
+    name: 'PINEAPPLE & EGG FRIED RICE',
+    description: 'spiced shredded chicken, cashew, mint & chilli',
+    price: '$25.5',
+    allergy: 'diary & gluten free'
+  },
+  {
+    id: '1',
+    img: 'stir-fry.PNG',
+    name: 'STIR FRY OF BUG TAIL & CHOPPED PRAWN',
+    description: 'egg noodle & hellfire chilli oil',
+    price: '$28.5',
+    allergy: 'diary free'
+  },
+  {
+    id: '2',
+    img: 'stir-fry.PNG',
+    name: 'STIR FRY OF BUG TAIL & CHOPPED PRAWN',
+    description: 'egg noodle & hellfire chilli oil',
+    price: '$28.5',
+    allergy: 'diary free'
+  },
+  {
+    id: '3',
+    img: 'crispy-soft.PNG',
+    name: 'CRISPY SOFT SHELL CRAB',
+    description: 'green curry fried rice, thai basil',
+    price: '$26.5',
+    allergy: 'diary free'
+  }
+];
