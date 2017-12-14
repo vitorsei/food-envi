98bf5e18c867d0d44a06abab2d93389964df3e67
vitoriwamura <seiji.vitor@gmail.com>
2017-12-14 14:24:30 +1100

    Creating home page

--- js/index.js
+++ js/index.js
@@ -3,16 +3,41 @@ var isScrolling;
 
 $(document).ready(function () {
   loadDetails();
+
+  $("#container1").click(function () {
+    window.location.href = 'details.html';
+  });
+
+  $("#container2").click(function () {
+    window.location.href = 'details.html';
+  });
+
+  $("#container3").click(function () {
+    window.location.href = 'details.html';
+  });
 });
 
-
-
 function loadDetails() {
-  $("#img1").attr("src", "./img/fried-rice.PNG");
-  $("#img2").attr("src", "./img/fried-rice.PNG");
-  $("#img3").attr("src", "./img/fried-rice.PNG");
+  $("#header").text("CHIN CHIN");
+
+  $("#bigImage1").attr("src", "./img/fried-rice.PNG");
+  $("#smallImageA1").attr("src", "./img/stir-fry.PNG");
+  $("#smallImageB1").attr("src", "./img/crispy-soft.PNG");
   $("#category1").text("RICE & NOODLES");
   $("#photosLabel1").text("4 dishes");
+
+
+  $("#bigImage2").attr("src", "./img/stir-fry.PNG");
+  $("#smallImageA2").attr("src", "./img/pad-thai.PNG");
+  $("#smallImageB2").attr("src", "./img/fried-rice.PNG");
+  $("#category2").text("GREEN STUFF");
+  $("#photosLabel2").text("3 dishes");
+
+  $("#bigImage3").attr("src", "./img/pad-thai.PNG");
+  $("#smallImageA3").attr("src", "./img/crispy-soft.PNG");
+  $("#smallImageB3").attr("src", "./img/stir-fry.PNG");
+  $("#category3").text("CURRIES");
+  $("#photosLabel3").text("3 dishes");
 }
 
 
